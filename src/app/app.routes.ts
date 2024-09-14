98312bed0e2da00623326216206b886458728ae6
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-14 18:45:44 -0300

    adicionando rotas da pagina home e da pagina login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './component/login/login.component';
 
-export const routes: Routes = [{
-    path: '',
-    component:  HomeComponent
-}];
+export const routes: Routes = [
+    {
+    path: '', redirectTo: "login", pathMatch:'full',
+    },
+    {
+        path: "home",
+        component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+    
+
+];
