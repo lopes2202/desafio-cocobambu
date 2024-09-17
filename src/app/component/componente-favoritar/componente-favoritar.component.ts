2969c91edd699b16875c14d6d7e318283265aee3
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-17 18:31:09 -0300

    adicionada uma função de adicionartag ao livro e ajustada a função de pesquisar, puxando as tags

--- src/app/component/componente-favoritar/componente-favoritar.component.ts
+++ src/app/component/componente-favoritar/componente-favoritar.component.ts
@@ -1,31 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Livro } from '../../models/livro'; 
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Livro } from '../../models/livro';
 import { BackendService } from '../../services/backend.service';
 import { GoogleLivrosService } from '../../services/google-livros.service';
 
 @Component({
   selector: 'app-componente-favoritar',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule,],
   templateUrl: './componente-favoritar.component.html',
   styleUrls: ['./componente-favoritar.component.css']
 })
 export class ComponenteFavoritarComponent implements OnInit {
   vetor: Livro[] = [];
 
-  constructor(private backendService: BackendService, private googleService: GoogleLivrosService, private router: Router ) {}
+  formularioTag = new FormGroup({
+    livroTag: new FormControl(''),
+
+  })
+
+  constructor(private backendService: BackendService, private googleService: GoogleLivrosService, private router: Router) { }
 
   ngOnInit(): void {
-    if(!localStorage.getItem('jwt_token')){
+    if (!localStorage.getItem('jwt_token')) {
       this.router.navigate(['/', 'login'])
     }
+    this.listarLivros()
+  }
 
-    this.backendService.listarLivros().subscribe({
+  listarLivros(){
+    this.vetor = []
+    this.backendService.listarLivros(this.formularioTag.value.livroTag).subscribe({
       next: (response) => {
-        for(let item of response){
+        for (let item of response) {
           this.googleService.getLivro(item.id_google).subscribe({
             next: (responseGoogle) => {
               let livro = new Livro()
@@ -35,19 +44,22 @@ export class ComponenteFavoritarComponent implements OnInit {
               livro.capaUrl = responseGoogle.volumeInfo.imageLinks.thumbnail;
               livro.nota = item.nota;
               livro.notasPessoais = item.notas_pessoais;
-              livro.tags = item.tags;
+              livro.tags = [];
+
+              for (let tag of item.tags) {
+                livro.tags.push(tag.nome)
+              }
               this.vetor.push(livro)
             }
           });
         }
       },
       error(err) {
-          
+
       },
     })
-
+  }
 
 }
 
-  
-}
+
