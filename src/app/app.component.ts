650ae474cfd960216a4b79b5d747bc4398edd69a
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-16 21:33:52 -0300

    ajustando o app raiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
-import { FirstcomponentComponent } from "./component/componentFormulario/componenteFormulario.component";
+import { ComponenteFormulario } from "./component/componentFormulario/componenteFormulario.component";
+import { ComponenteFavoritarComponent } from "./component/componente-favoritar/componente-favoritar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FirstcomponentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './component/login/login.component';
+import { ComponenteFavoritarComponent } from './component/componente-favoritar/componente-favoritar.component';
 
 export const routes: Routes = [
     {
-    path: '', redirectTo: "login", pathMatch:'full',
-    },
-    {
-        path: "home",
-        component: HomeComponent
-    },
-    {
+        path: '', 
+        redirectTo: 'login', 
+        pathMatch: 'full'
+      },
+      {
         path: 'login',
         component: LoginComponent
-    }
-    
-
-];
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'livrofavoritado',
+        component: ComponenteFavoritarComponent 
+      },
+    ];
