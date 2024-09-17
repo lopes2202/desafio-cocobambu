49826fd236219fa9617681973d9142aa4c720b04
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2024-09-16 21:35:26 -0300

    ajustando o home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { GoogleLivrosService } from '../../services/google-livros.service';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ComponenteFavoritarComponent } from "../../component/componente-favoritar/componente-favoritar.component";
+import { ComponenteFormulario } from '../../component/componentFormulario/componenteFormulario.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, ComponenteFormulario],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
